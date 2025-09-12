effd471cfad22f654d01866480a70c049c4b1751
vathsa ponnappa <vathsalingeri@gmail.com>
2025-09-12 02:27:06 +0000

    resloved linting in test file

--- test/test_main.py
+++ test/test_main.py
@@ -1,14 +1,9 @@
-import sys
-import os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from main import add, sub
 
 
-from app.main import add,sub
+def test_addition():
+    assert add(10, 5) == 15
 
 
-def test_add():
-    assert add(2, 3) == 5
-
-
-def test_sub():
-    assert sub(5, 3) == 2
+def test_subtaction():
+    assert sub(10, 5) == 5
