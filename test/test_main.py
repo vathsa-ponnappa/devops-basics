7e14d80cbb84eae3c1b6431c5f739f8716d61a7a
vathsa ponnappa <vathsalingeri@gmail.com>
2025-09-12 02:23:31 +0000

    resolved linting issues in test file

--- test/test_main.py
+++ test/test_main.py
@@ -2,10 +2,13 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+
 from app.main import add,sub
+
 
 def test_add():
     assert add(2, 3) == 5
+
 
 def test_sub():
     assert sub(5, 3) == 2
