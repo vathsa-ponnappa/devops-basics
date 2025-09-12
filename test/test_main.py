f020523bae3e622b459b7a35360831bfbb81d216
vathsa ponnappa <vathsalingeri@gmail.com>
2025-09-12 01:27:54 +0000

    fixed indentation issue in main.py

--- test/test_main.py
+++ test/test_main.py
@@ -1,7 +1,7 @@
-from app.main import add
+from app.main import add,sub
 
 def test_add():
     assert add(2, 3) == 5
 
-def test-sub():
-    assert sub(5,3) == 2
+def test_sub():
+    assert sub(5, 3) == 2
